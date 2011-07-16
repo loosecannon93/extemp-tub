ac23b6bef158fbddb99722bdf6658135a513146b
Cannon Matthews <cannon@cannonmatthews.com>
2011-07-16 03:59:35 -0500

    seperated out users and accoutns controllers so i can create neww ones and destroy them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_filter :require_user
-  
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
@@ -9,27 +11,36 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:notice] = "Account registered!"
-      redirect_back_or_default account_url
+      redirect_back_or_default users_url
     else
       render :action => :new
     end
   end
   
   def show
-    @user = @current_user
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user = @current_user
+    @user = User.find(params[:id])
   end
   
   def update
-    @user = @current_user # makes our views "cleaner" and more consistent
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:notice] = "Account updated!"
       redirect_to account_url
     else
       render :action => :edit
+    end
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to(users_url) }
+      format.xml  { head :ok }
     end
   end
 end
